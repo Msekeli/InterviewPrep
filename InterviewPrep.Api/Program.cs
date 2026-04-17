b37bbbd3fbd5206bfa9aa1d3112fb97ff23086d8
Msekeli <msekeli14@gmail.com>
2026-04-17 02:11:01 +0200

    Update package references and enhance OpenAPI configuration in Program.cs

--- InterviewPrep.Api/Program.cs
+++ InterviewPrep.Api/Program.cs
@@ -1,23 +1,38 @@
+using InterviewPrep.Application.Interfaces;
+using InterviewPrep.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+
+var home = Environment.GetEnvironmentVariable("HOME");
+var dataFolder = Path.Combine(home ?? builder.Environment.ContentRootPath, "Data");
+Directory.CreateDirectory(dataFolder);
+var dbPath = Path.Combine(dataFolder, "interviewprep.db");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite($"Data Source={dbPath}"));
+
+builder.Services.AddScoped<IInterviewSessionRepository, InterviewSessionRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "InterviewPrep API v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
